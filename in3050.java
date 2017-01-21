c41588f2f9d660b185fbf06dcde70a81ecb41d86
Naya <nayayoung92@gmail.com>
2017-01-21 18:55:10 -0500

    Update in3050.java

--- in3050.java
+++ in3050.java
@@ -1,3 +1,6 @@
+// Given 2 int values, return true if they are both in the range 30..40 inclusive, 
+// or they are both in the range 40..50 inclusive.
+
 public class Main {
   public boolean in3050(int a, int b) {
     boolean range30 = (a >= 30 && a <= 40) && (b >= 30 && b <= 40);
