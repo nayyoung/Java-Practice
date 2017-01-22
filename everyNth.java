c0b24db7647150a9be1e6e3f4781563e51dd06a0
Naya <nayayoung92@gmail.com>
2017-01-21 19:35:41 -0500

    Create everyNth.java

---
+++ everyNth.java
@@ -0,0 +1,14 @@
+// Given a non-empty string and an int N, return the string 
+// made starting with char 0, 
+// and then every Nth char of the string. So if N is 3, use char 0, 3, 6, ... and so on. 
+// N is 1 or more.
+
+public class EveryNth {
+  public String everyNth(String str, int n) {
+    String complete = "";
+    for (int i = 0; i < str.length(); i += n) {
+      complete += str.charAt(i);
+    }
+    return complete;
+  }
+}
